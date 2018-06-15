7188fc54d9ea1dcd207eeaeafaab088c4f91780c
Roma Sokolov <sokolov.r.v@gmail.com>
2018-06-15 14:59:19 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+#![deny(warnings)]
+#![no_std]
+#![no_main]
+
+extern crate cortex_m;
+#[macro_use]
+extern crate cortex_m_rt as rt;
+extern crate panic_abort;
+
+use rt::ExceptionFrame;
+
+entry!(main);
+fn main() -> !{
+    panic!("opa");
+}
+
+exception!(HardFault, hard_fault);
+fn hard_fault(ef: &ExceptionFrame) -> ! {
+    panic!("HardFault at {:#?}", ef);
+}
+
+exception!(*, default_handler);
+fn default_handler(irqn: i16) {
+    panic!("Unhandled exception (IRQn = {})", irqn);
+}
