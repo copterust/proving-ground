8b92300a0aff5e359b2c245745e9996e0ceeb9f3
Alexander Zhuravlev <alexander.zhouravlev@gmail.com>
2018-06-23 13:04:46 +0200

    Configure porta as af6 output

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,24 @@ extern crate cortex_m;
 extern crate cortex_m_rt as rt;
 extern crate panic_abort;
 
+extern crate stm32f30x;
+
 use rt::ExceptionFrame;
 
 entry!(main);
 fn main() -> !{
-    panic!("opa");
+    let prphs = stm32f30x::Peripherals::take().unwrap();
+    prphs.RCC.apb2enr.write(|w| w.tim1en().enabled());
+    prphs.RCC.ahbenr.write(|w| w.iopaen().enabled());
+
+    prphs.GPIOA.moder.write(|w| w.moder8().alternate());
+    unsafe {
+        prphs.GPIOA.afrh.write(|w| w.afrh8().bits(0b0000_0110));
+    }
+
+    loop {
+        
+    }
 }
 
 exception!(HardFault, hard_fault);
