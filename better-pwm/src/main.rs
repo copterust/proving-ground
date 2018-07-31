9cd669ce4ee0ab087a523156202bfec07f582ba3
Alexander Zhuravlev <alexander.zhouravlev@gmail.com>
2018-07-31 22:25:45 +0200

    Change PWM pin to PB8 and increase range

--- better-pwm/src/main.rs
+++ better-pwm/src/main.rs
@@ -12,7 +12,7 @@ extern crate alt_stm32f30x_hal as hal;
 extern crate stm32f30x;
 
 use hal::gpio;
-use hal::gpio::gpiob::PB6;
+use hal::gpio::gpiob::PB8;
 use hal::prelude::*;
 use hal::pwm::PwmBinding;
 use hal::timer;
@@ -30,28 +30,28 @@ fn main() -> ! {
 
     let gpiob = device.GPIOB.split(&mut rcc.ahb);
     // get port b
-    let pb6 = gpiob.pb6.pull_type(gpio::PullUp);
+    let pb8 = gpiob.pb8.pull_type(gpio::PullUp);
     let clocks = rcc
         .cfgr
         .sysclk(64.mhz())
         .pclk1(32.mhz())
         .freeze(&mut flash.acr);
     let tim4 = timer::tim4::Timer::new(device.TIM4, 1.mhz(), clocks, &mut rcc.apb1);
-    let (ch1, mut tim4) = tim4.take_ch1();
+    let (ch3, mut tim4) = tim4.take_ch3();
     // Two ways to create binding: via named func or via turbo fishing:
     // let mut pwm = PwmBinding::bind_pb6_tim4_ch1(pb6, ch1);
-    let mut pwm = PwmBinding::<PB6<_, _>, tim4::Channel<timer::CH1, _>, gpio::AF2>::new(pb6, ch1);
+    let mut pwm = PwmBinding::<PB8<_, _>, tim4::Channel<timer::CH3, _>, gpio::AF2>::new(pb8, ch3);
     pwm.enable();
     tim4.enable();
 
     loop {
-        for i in 10..50 {
+        for i in 10..500 {
             pwm.set_duty(i);
-            tick_delay(25000);
+            tick_delay(2500);
         }
-        for i in 10..50 {
-            pwm.set_duty(50 - i);
-            tick_delay(25000);
+        for i in 10..500 {
+            pwm.set_duty(500 - i);
+            tick_delay(2500);
         }
     }
 }
