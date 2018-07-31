32c27c5d4670359e5ea0a5e361e4438235c41a18
Roma Sokolov <sokolov.r.v@gmail.com>
2018-07-31 22:48:14 +0200

    Set periods back to 50; use tim2 and pa0

--- better-pwm/src/main.rs
+++ better-pwm/src/main.rs
@@ -12,11 +12,11 @@ extern crate alt_stm32f30x_hal as hal;
 extern crate stm32f30x;
 
 use hal::gpio;
-use hal::gpio::gpiob::PB8;
+use hal::gpio::gpioa::PA0;
 use hal::prelude::*;
 use hal::pwm::PwmBinding;
 use hal::timer;
-use hal::timer::tim4;
+use hal::timer::tim2;
 
 use rt::ExceptionFrame;
 
@@ -28,30 +28,30 @@ fn main() -> ! {
     let mut flash = device.FLASH.constrain();
     let mut rcc = device.RCC.constrain();
 
-    let gpiob = device.GPIOB.split(&mut rcc.ahb);
+    let gpioa = device.GPIOA.split(&mut rcc.ahb);
     // get port b
-    let pb8 = gpiob.pb8.pull_type(gpio::PullUp);
+    let pin = gpioa.pa0.pull_type(gpio::PullUp);
     let clocks = rcc
         .cfgr
         .sysclk(64.mhz())
         .pclk1(32.mhz())
         .freeze(&mut flash.acr);
-    let tim4 = timer::tim4::Timer::new(device.TIM4, 1.mhz(), clocks, &mut rcc.apb1);
-    let (ch3, mut tim4) = tim4.take_ch3();
+    let tim = timer::tim2::Timer::new(device.TIM2, 1.mhz(), clocks, &mut rcc.apb1);
+    let (ch, mut tim) = tim.take_ch1();
     // Two ways to create binding: via named func or via turbo fishing:
     // let mut pwm = PwmBinding::bind_pb6_tim4_ch1(pb6, ch1);
-    let mut pwm = PwmBinding::<PB8<_, _>, tim4::Channel<timer::CH3, _>, gpio::AF2>::new(pb8, ch3);
+    let mut pwm = PwmBinding::<PA0<_, _>, tim2::Channel<timer::CH1, _>, gpio::AF1>::new(pin, ch);
     pwm.enable();
-    tim4.enable();
+    tim.enable();
 
     loop {
-        for i in 10..500 {
+        for i in 10..50 {
             pwm.set_duty(i);
-            tick_delay(2500);
+            tick_delay(25000);
         }
-        for i in 10..500 {
-            pwm.set_duty(500 - i);
-            tick_delay(2500);
+        for i in 10..50 {
+            pwm.set_duty(50 - i);
+            tick_delay(25000);
         }
     }
 }
