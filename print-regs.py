c98491ea32bc765f503e6dfedee9872e40cdf69e
Roma Sokolov <sokolov.r.v@gmail.com>
2018-07-31 23:58:37 +0200

    Add regs for tim4

--- print-regs.py
+++ print-regs.py
@@ -2,8 +2,13 @@
 
 import sys
 
-REGS = ['moder', 'otyper', 'ospeedr', 'pupdr',
-        'idr', 'odr', 'bsrr', 'lckr', 'afrl', 'afrh', 'brr']
+REGS = {
+     'gpiob': ['moder', 'otyper', 'ospeedr', 'pupdr',
+               'idr', 'odr', 'bsrr', 'lckr', 'afrl', 'afrh', 'brr'],
+     'tim4': ['cr1', 'cr2', 'smcr', 'dier',
+              'sr', 'egr', 'ccmr1_output', 'ccmr2_output', 'ccer',
+              'cnt', 'psc', 'arr', 'rsrv', 'ccr1', 'ccr2', 'ccr3', 'ccr4', 'dcr']
+}
 
 def sreg(fn):
      z = open(fn,  "rb").read()
@@ -13,6 +18,7 @@ def sreg(fn):
          s = ''.join(bin(b)[2:].rjust(8, '0') for b in buf)
          yield s
 
-fn = sys.argv[1]
-for (state, reg) in zip(sreg(fn), REGS):
+r = sys.argv[1]
+fn = sys.argv[2]
+for (state, reg) in zip(sreg(fn), REGS.get(r)):
     print('{}: {}'.format(reg.rjust(12, ' '), state))
