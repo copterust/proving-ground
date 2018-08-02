7c9c42bc7fbab7c1703611b6dbe38ec6b62148f7
Roma Sokolov <sokolov.r.v@gmail.com>
2018-08-02 22:57:49 +0200

    Back to nightly

--- travis.sh
+++ travis.sh
@@ -3,6 +3,6 @@
 for dir in `ls -d *`; do
     if [ -d $dir ]; then
         echo "Building $dir"
-        cd $dir && cargo build --verbose ; cd ..
+        cd $dir && cargo build --verbose || exit 1; cd ..
     fi
 done
