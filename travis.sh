303ac60dd5aa5aaa79d3fe017e10d4ee80fdf514
Roma Sokolov <sokolov.r.v@gmail.com>
2018-08-02 23:08:49 +0200

    Fix

--- travis.sh
+++ travis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for dir in `ls -d *`; do
-    if [ -d $dir ]; then
+    if [ -d $dir ] && [ -f $dir/Cargo.toml ] ; then
         echo "Building $dir"
         cd $dir && cargo build --verbose || exit 1; cd ..
     fi
