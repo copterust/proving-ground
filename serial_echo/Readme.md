e549f9c0d7689546ce15ae15c55c76d05e05bdf5
Roma Sokolov <sokolov.r.v@gmail.com>
2018-08-07 21:54:20 +0200

    Add serial echo example

---
+++ serial_echo/Readme.md
@@ -0,0 +1,5 @@
+# Serial echo
+
+Example of serial interface reading/writing; using core::fmt macros.
+
+One can use serial-to-usb converter and minicom to chat: `minicom -D /dev/tty.usbserial-A20027Ve -b 9600`.
---
+++ serial_echo/src/main.rs
@@ -0,0 +1,99 @@
+#![deny(warnings)]
+#![no_std]
+#![no_main]
+
+#[allow(unused)]
+use panic_abort;
+
+use core::fmt::{self, Write};
+
+use hal::gpio;
+use hal::prelude::*;
+use hal::serial;
+use hal::time::Bps;
+use nb;
+use rt::{entry, exception, ExceptionFrame};
+
+entry!(main);
+fn main() -> ! {
+    let device = hal::stm32f30x::Peripherals::take().unwrap();
+    let mut rcc = device.RCC.constrain();
+    let mut flash = device.FLASH.constrain();
+    let clocks = rcc
+        .cfgr
+        .sysclk(64.mhz())
+        .pclk1(32.mhz())
+        .pclk2(36.mhz())
+        .freeze(&mut flash.acr);
+    let gpioa = device.GPIOA.split(&mut rcc.ahb);
+
+    let txpin = gpioa.pa9.alternating(gpio::AF7);
+    let rxpin = gpioa.pa10.alternating(gpio::AF7);
+    let mut serial = serial::Serial::usart1(
+        device.USART1,
+        (txpin, rxpin),
+        Bps(9600),
+        clocks,
+        &mut rcc.apb2,
+    );
+    serial.listen(serial::Event::Rxne);
+    let (mut tx, mut rx) = serial.split();
+    // COBS frame
+    tx.write(0x00).unwrap();
+    let mut l = Logger { tx };
+    write!(l, "starting loop...\r\n");
+    loop {
+        match rx.read() {
+            Ok(b) => {
+                l.tx.write(b).unwrap();
+            }
+            Err(nb::Error::Other(e)) => match e {
+                serial::Error::Overrun => {
+                    rx.clear_overrun_error();
+                }
+                _ => {
+                    write!(l, "read error: {:?}", e);
+                }
+            },
+            Err(nb::Error::WouldBlock) => {}
+        };
+    }
+}
+
+struct Logger<W: ehal::serial::Write<u8>> {
+    tx: W,
+}
+impl<W: ehal::serial::Write<u8>> fmt::Write for Logger<W> {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        for c in s.chars() {
+            match self.write_char(c) {
+                Ok(_) => {}
+                Err(_) => {}
+            }
+        }
+        match self.tx.flush() {
+            Ok(_) => {}
+            Err(_) => {}
+        };
+
+        Ok(())
+    }
+
+    fn write_char(&mut self, s: char) -> fmt::Result {
+        match nb::block!(self.tx.write(s as u8)) {
+            Ok(_) => {}
+            Err(_) => {}
+        }
+        Ok(())
+    }
+}
+
+exception!(HardFault, hard_fault);
+fn hard_fault(ef: &ExceptionFrame) -> ! {
+    panic!("HardFault at {:#?}", ef);
+}
+
+exception!(*, default_handler);
+fn default_handler(irqn: i16) {
+    panic!("Unhandled exception (IRQn = {})", irqn);
+}
