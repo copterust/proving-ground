3bc923af04c4659d5730775483e4f3c6ac0c0901
Alexander Zhuravlev <alexander.zhouravlev@gmail.com>
2018-08-17 19:23:06 +0200

    Read status register

--- bmp288/src/bmp288.rs
+++ bmp288/src/bmp288.rs
@@ -1,6 +1,7 @@
 #![no_std]
 
 //use ehal::blocking::i2c::WriteRead;
+use core::fmt;
 
 pub const ADDRESS: u8 = 0x76;
 
@@ -20,26 +21,50 @@ where I2C: ehal::blocking::i2c::WriteRead<Error = E> {
 
 impl<I2C: ehal::blocking::i2c::WriteRead> BMP288<I2C>
 {
-    pub fn id(&mut self) -> u8 {
-        self.read_byte(Register::ID)
+    pub fn status(&mut self) -> Status {
+        let status = self.read_byte(Register::status);
+        Status {
+            measuring: (1 == status & 0b00001000),
+            im_update: (1 == status & 0b00000001)
+        }
     }
 
-/*
+    pub fn id(&mut self) -> u8 {
+        self.read_byte(Register::id)
+    }
+
+    /// Software reset, emulates POR
+    pub fn reset(&mut self) {
+        self.write_byte(Register::reset, 0xB6); // Magic from documentation
+    }
+
     fn write_byte(&mut self, reg: Register, byte: u8) {
         let mut buffer = [0];
         let _ = self.com.write_read(ADDRESS, &[reg as u8, byte], &mut buffer);
     }
-*/
+
     fn read_byte(&mut self, reg: Register) -> u8 {
         let mut data: [u8; 1] = [0];
-        // FIXME:
-        //  * device address is not a const
-        //  * register address is u16
         let _ = self.com.write_read(ADDRESS, &[reg as u8], &mut data);
         data[0]
     }
 }
 
+pub struct Status {
+    measuring: bool,
+    im_update: bool
+}
+
+impl fmt::Display for Status {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "conversion is running: {}, NVM data being copied: {}",
+            self.measuring, self.im_update)
+    }
+}
+
+#[allow(non_camel_case_types)]
 enum Register {
-    ID = 0xD0,
+    id = 0xD0,
+    reset = 0xE0,
+    status = 0xF3,
 }
---
+++ bmp288/src/main.rs
@@ -0,0 +1,119 @@
+#![deny(warnings)]
+#![no_std]
+#![no_main]
+
+#[allow(unused)]
+use panic_abort;
+
+use core::fmt::{self, Write};
+
+use hal::gpio;
+use hal::prelude::*;
+use hal::serial;
+use hal::time::Bps;
+use nb;
+use rt::{entry, exception, ExceptionFrame};
+
+mod bmp288;
+
+entry!(main);
+fn main() -> ! {
+    let device = hal::stm32f30x::Peripherals::take().unwrap();
+    let mut rcc = device.RCC.constrain();
+    let mut flash = device.FLASH.constrain();
+    let clocks = rcc
+        .cfgr
+        .sysclk(64.mhz())
+        .pclk1(32.mhz())
+        .pclk2(36.mhz())
+        .freeze(&mut flash.acr);
+
+    // serial
+    let gpioa = device.GPIOA.split(&mut rcc.ahb);
+    let txpin = gpioa.pa9.alternating(gpio::AF7);
+    let rxpin = gpioa.pa10.alternating(gpio::AF7);
+    let mut serial = serial::Serial::usart1(
+        device.USART1,
+        (txpin, rxpin),
+        Bps(9600),
+        clocks,
+        &mut rcc.apb2,
+    );
+    serial.listen(serial::Event::Rxne);
+    let (tx, mut rx) = serial.split();
+
+    let mut l = Logger { tx };
+    write!(l, "\r\nBMP288 demo\r\n");
+
+    // i2c
+    let gpiob = device.GPIOB.split(&mut rcc.ahb);
+    let scl = gpiob.pb8.alternating(gpio::AF4);
+    let sda = gpiob.pb9.alternating(gpio::AF4);
+    let i2c = hal::i2c::I2c::i2c1(
+        device.I2C1,
+        (scl, sda),
+        1.mhz(),
+        clocks,
+        &mut rcc.apb1);
+
+    let mut ps = bmp288::new(i2c).unwrap();
+    write!(l, "ID: {}\r\n", ps.id());
+    ps.reset();
+    write!(l, "ID after reset: {}\r\n", ps.id());
+    write!(l, "Status: {}\r\n", ps.status());
+
+    loop {
+        match rx.read() {
+            Ok(b) => {
+                l.tx.write(b).unwrap();
+            }
+            Err(nb::Error::Other(e)) => match e {
+                serial::Error::Overrun => {
+                    rx.clear_overrun_error();
+                }
+                _ => {
+                    write!(l, "read error: {:?}", e);
+                }
+            },
+            Err(nb::Error::WouldBlock) => {}
+        };
+    }
+}
+
+struct Logger<W: ehal::serial::Write<u8>> {
+    tx: W,
+}
+impl<W: ehal::serial::Write<u8>> fmt::Write for Logger<W> {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        for c in s.chars() {
+            match self.write_char(c) {
+                Ok(_) => {}
+                Err(_) => {}
+            }
+        }
+        match self.tx.flush() {
+            Ok(_) => {}
+            Err(_) => {}
+        };
+
+        Ok(())
+    }
+
+    fn write_char(&mut self, s: char) -> fmt::Result {
+        match nb::block!(self.tx.write(s as u8)) {
+            Ok(_) => {}
+            Err(_) => {}
+        }
+        Ok(())
+    }
+}
+
+exception!(HardFault, hard_fault);
+fn hard_fault(ef: &ExceptionFrame) -> ! {
+    panic!("HardFault at {:#?}", ef);
+}
+
+exception!(*, default_handler);
+fn default_handler(irqn: i16) {
+    panic!("Unhandled exception (IRQn = {})", irqn);
+}
