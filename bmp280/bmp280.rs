37dc4750fd84b7510d746e9c9faa40fd212e7ebb
Roma Sokolov <sokolov.r.v@gmail.com>
2018-08-19 23:41:43 +0200

    Fix bmp and also accidently apply rustfmt %)

--- bmp280/bmp280.rs
+++ bmp280/bmp280.rs
@@ -1,12 +1,9 @@
-#![no_std]
-
 //use ehal::blocking::i2c::WriteRead;
 use core::fmt;
 
 pub const ADDRESS: u8 = 0x76;
 
-pub struct BMP280<I2C: ehal::blocking::i2c::WriteRead>
-{
+pub struct BMP280<I2C: ehal::blocking::i2c::WriteRead> {
     com: I2C,
     // Temperature compensation
     dig_t1: u16,
@@ -22,11 +19,13 @@ pub struct BMP280<I2C: ehal::blocking::i2c::WriteRead>
     dig_p6: i16,
     dig_p7: i16,
     dig_p8: i16,
-    dig_p9: i16
+    dig_p9: i16,
 }
 
 pub fn new<I2C, E>(i2c: I2C) -> Result<BMP280<I2C>, E>
-where I2C: ehal::blocking::i2c::WriteRead<Error = E> {
+where
+    I2C: ehal::blocking::i2c::WriteRead<Error = E>,
+{
     let mut chip = BMP280 {
         com: i2c,
         dig_t1: 0,
@@ -41,7 +40,7 @@ where I2C: ehal::blocking::i2c::WriteRead<Error = E> {
         dig_p6: 0,
         dig_p7: 0,
         dig_p8: 0,
-        dig_p9: 0
+        dig_p9: 0,
     };
 
     if chip.id() == 0x58 {
@@ -51,11 +50,12 @@ where I2C: ehal::blocking::i2c::WriteRead<Error = E> {
     Ok(chip)
 }
 
-impl<I2C: ehal::blocking::i2c::WriteRead> BMP280<I2C>
-{
+impl<I2C: ehal::blocking::i2c::WriteRead> BMP280<I2C> {
     fn read_calibration(&mut self) {
         let mut data: [u8; 24] = [0; 24];
-        let _ = self.com.write_read(ADDRESS, &[Register::calib00 as u8], &mut data);
+        let _ = self
+            .com
+            .write_read(ADDRESS, &[Register::calib00 as u8], &mut data);
 
         self.dig_t1 = ((data[1] as u16) << 8) | (data[0] as u16);
         self.dig_t2 = ((data[3] as i16) << 8) | (data[2] as i16);
@@ -74,15 +74,17 @@ impl<I2C: ehal::blocking::i2c::WriteRead> BMP280<I2C>
 
     pub fn read_pres(&mut self) -> f64 {
         let mut data: [u8; 6] = [0, 0, 0, 0, 0, 0];
-        let _ = self.com.write_read(ADDRESS, &[Register::press as u8], &mut data);
+        let _ = self
+            .com
+            .write_read(ADDRESS, &[Register::press as u8], &mut data);
         let press = (data[0] as u32) << 12 | (data[1] as u32) << 4 | (data[2] as u32) >> 4;
 
         let mut var1 = ((self.t_fine as f64) / 2.0) - 64000.0;
         let mut var2 = var1 * var1 * (self.dig_p6 as f64) / 32768.0;
         var2 = var2 + var1 * (self.dig_p5 as f64) * 2.0;
         var2 = (var2 / 4.0) + ((self.dig_p4 as f64) * 65536.0);
-        var1 = ((self.dig_p3 as f64) * var1 * var1 / 524288.0
-                + (self.dig_p2 as f64) * var1) / 524288.0;
+        var1 = ((self.dig_p3 as f64) * var1 * var1 / 524288.0 + (self.dig_p2 as f64) * var1)
+            / 524288.0;
         var1 = (1.0 + var1 / 32768.0) * (self.dig_p1 as f64);
         let mut pressure = 1048576.0 - (press as f64);
         if var1 != 0.0 {
@@ -96,17 +98,16 @@ impl<I2C: ehal::blocking::i2c::WriteRead> BMP280<I2C>
 
     pub fn read_temp(&mut self) -> f64 {
         let mut data: [u8; 6] = [0, 0, 0, 0, 0, 0];
-        let _ = self.com.write_read(ADDRESS, &[Register::press as u8], &mut data);
-        let _pres = (data[0] as u32) << 12 | (data[1] as u32) << 4
-            | (data[2] as u32) >> 4;
-        let temp = (data[3] as u32) << 12 | (data[4] as u32) << 4
-            | (data[5] as u32) >> 4;
+        let _ = self
+            .com
+            .write_read(ADDRESS, &[Register::press as u8], &mut data);
+        let _pres = (data[0] as u32) << 12 | (data[1] as u32) << 4 | (data[2] as u32) >> 4;
+        let temp = (data[3] as u32) << 12 | (data[4] as u32) << 4 | (data[5] as u32) >> 4;
 
-        let v1 = ((temp as f64) / 16384.0 - (self.dig_t1 as f64) / 1024.0)
-                * (self.dig_t2 as f64);
+        let v1 = ((temp as f64) / 16384.0 - (self.dig_t1 as f64) / 1024.0) * (self.dig_t2 as f64);
         let v2 = (((temp as f64) / 131072.0 - (self.dig_t1 as f64) / 8192.0)
-                * ((temp as f64) / 131072.0 - (self.dig_t1 as f64) / 8192.0))
-                * (self.dig_t3 as f64);
+            * ((temp as f64) / 131072.0 - (self.dig_t1 as f64) / 8192.0))
+            * (self.dig_t3 as f64);
         self.t_fine = (v1 + v2) as i32;
 
         ((v1 + v2) / 5120.0)
@@ -123,7 +124,7 @@ impl<I2C: ehal::blocking::i2c::WriteRead> BMP280<I2C>
             x if x == Standby::ms1000 as u8 => Standby::ms1000,
             x if x == Standby::ms2000 as u8 => Standby::ms2000,
             x if x == Standby::ms4000 as u8 => Standby::ms4000,
-            _ => Standby::unknown
+            _ => Standby::unknown,
         };
         let filter = match (config & (0b111 << 2)) >> 2 {
             x if x == Filter::off as u8 => Filter::off,
@@ -131,9 +132,12 @@ impl<I2C: ehal::blocking::i2c::WriteRead> BMP280<I2C>
             x if x == Filter::c4 as u8 => Filter::c4,
             x if x == Filter::c8 as u8 => Filter::c8,
             x if x == Filter::c16 as u8 => Filter::c16,
-            _ => Filter::unknown
+            _ => Filter::unknown,
         };
-        Config { t_sb: t_sb, filter: filter }
+        Config {
+            t_sb: t_sb,
+            filter: filter,
+        }
     }
 
     pub fn set_config(&mut self, new: Config) {
@@ -158,7 +162,7 @@ impl<I2C: ehal::blocking::i2c::WriteRead> BMP280<I2C>
             x if x == Oversampling::x2 as u8 => Oversampling::x2,
             x if x == Oversampling::x4 as u8 => Oversampling::x4,
             x if x == Oversampling::x8 as u8 => Oversampling::x8,
-            _ => Oversampling::x16
+            _ => Oversampling::x16,
         };
         let osrs_p = match (config & (0b111 << 2)) >> 2 {
             x if x == Oversampling::skipped as u8 => Oversampling::skipped,
@@ -166,23 +170,27 @@ impl<I2C: ehal::blocking::i2c::WriteRead> BMP280<I2C>
             x if x == Oversampling::x2 as u8 => Oversampling::x2,
             x if x == Oversampling::x4 as u8 => Oversampling::x4,
             x if x == Oversampling::x8 as u8 => Oversampling::x8,
-            _ => Oversampling::x16
+            _ => Oversampling::x16,
         };
         let mode = match config & 0b11 {
             x if x == PowerMode::Sleep as u8 => PowerMode::Sleep,
             x if x == PowerMode::Forced as u8 => PowerMode::Forced,
             x if x == PowerMode::Normal as u8 => PowerMode::Normal,
-            _ => PowerMode::Forced
+            _ => PowerMode::Forced,
         };
 
-        Control { osrs_t: osrs_t, osrs_p: osrs_p, mode: mode }
+        Control {
+            osrs_t: osrs_t,
+            osrs_p: osrs_p,
+            mode: mode,
+        }
     }
 
     pub fn status(&mut self) -> Status {
         let status = self.read_byte(Register::status);
         Status {
             measuring: (1 == status & 0b00001000),
-            im_update: (1 == status & 0b00000001)
+            im_update: (1 == status & 0b00000001),
         }
     }
 
@@ -197,7 +205,9 @@ impl<I2C: ehal::blocking::i2c::WriteRead> BMP280<I2C>
 
     fn write_byte(&mut self, reg: Register, byte: u8) {
         let mut buffer = [0];
-        let _ = self.com.write_read(ADDRESS, &[reg as u8, byte], &mut buffer);
+        let _ = self
+            .com
+            .write_read(ADDRESS, &[reg as u8, byte], &mut buffer);
     }
 
     fn read_byte(&mut self, reg: Register) -> u8 {
@@ -211,7 +221,7 @@ impl<I2C: ehal::blocking::i2c::WriteRead> BMP280<I2C>
 pub struct Control {
     pub osrs_t: Oversampling,
     pub osrs_p: Oversampling,
-    pub mode: PowerMode
+    pub mode: PowerMode,
 }
 
 /// Standby time in ms
@@ -226,7 +236,7 @@ pub enum Standby {
     ms1000 = 0b101,
     ms2000 = 0b110,
     ms4000 = 0b111,
-    unknown
+    unknown,
 }
 
 /// The time constant of IIR filter
@@ -238,7 +248,7 @@ pub enum Filter {
     c4 = 0x02,
     c8 = 0x03,
     c16 = 0x04,
-    unknown
+    unknown,
 }
 
 /// Configuration register, sets the rate, filter and interface options
@@ -251,18 +261,21 @@ pub struct Config {
     /// Controls inactive duration in normal mode
     pub t_sb: Standby,
     /// Controls the time constant of IIR filter
-    pub filter: Filter
+    pub filter: Filter,
 }
 
 pub struct Status {
     measuring: bool,
-    im_update: bool
+    im_update: bool,
 }
 
 impl fmt::Display for Status {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "conversion is running: {}, NVM data being copied: {}",
-            self.measuring, self.im_update)
+        write!(
+            f,
+            "conversion is running: {}, NVM data being copied: {}",
+            self.measuring, self.im_update
+        )
     }
 }
 
@@ -275,7 +288,7 @@ pub enum Oversampling {
     x2 = 0b010,
     x4 = 0b011,
     x8 = 0b100,
-    x16 = 0b101
+    x16 = 0b101,
 }
 
 #[derive(Debug)]
@@ -283,7 +296,7 @@ pub enum Oversampling {
 pub enum PowerMode {
     Sleep = 0b00,
     Forced = 0b01,
-    Normal = 0b11
+    Normal = 0b11,
 }
 
 #[allow(non_camel_case_types)]
@@ -294,5 +307,5 @@ enum Register {
     ctrl_meas = 0xF4,
     config = 0xF5,
     press = 0xF7,
-    calib00 = 0x88
+    calib00 = 0x88,
 }
