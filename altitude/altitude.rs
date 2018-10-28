0d3e576e8221e9cdcaae9da5e448946aa35dc4c4
Alexander <alexander@192.168.2.90>
2018-10-28 17:53:09 +0100

    Add function to convert baro to asl

--- altitude/altitude.rs
+++ altitude/altitude.rs
@@ -1,7 +1,7 @@
 /// Convert above the sea level to Pascals
 ///
 /// Returns:
-/// * p: air pressure above sea level
+/// * p: air pressure above sea level (Pa)
 /// Input:
 /// * h: altitude above sea level (m)
 ///
@@ -9,4 +9,14 @@
 /// * http://www.engineeringtoolbox.com/air-altitude-pressure-d_462.html
 fn asl_to_baro(h: f32) -> f32 {
     101325 * (1 - 2.25577e-5 * h).powf(5.25588)
+}
+
+/// Convert pressure to above the sea level
+///
+/// Returns:
+/// * h: altitude above sea level (m)
+/// Input:
+/// * p: air pressure
+fn baro_to_asl(p: f32) -> f32 {
+    (1.0 - (p / 101325.0).powf(0.190295)) * 44330.0
 }
