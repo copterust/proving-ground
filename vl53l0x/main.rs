7d0346fdf3053f344be12ed1c49677f4561cb8d0
Roma Sokolov <sokolov.r.v@gmail.com>
2018-10-30 20:51:49 +0100

    Improve vl test app

--- vl53l0x/main.rs
+++ vl53l0x/main.rs
@@ -1,23 +1,31 @@
 #![no_std]
 #![no_main]
-
-#[allow(unused)]
-use panic_abort;
+#![feature(core_intrinsics)]
 
 use core::fmt::{self, Write};
+use core::intrinsics;
+use core::panic::PanicInfo;
 
 use cortex_m_rt::{entry, exception, ExceptionFrame};
 use hal::gpio;
 use hal::prelude::*;
 use hal::serial;
+use hal::stm32f30x::interrupt;
 use hal::time::Bps;
 use nb;
 
 use vl53l0x;
 
+static mut L: Option<Logger<hal::serial::Tx<hal::stm32f30x::USART1>>> = None;
+static mut RX: Option<hal::serial::Rx<hal::stm32f30x::USART1>> = None;
+
+static mut QUIET: bool = false;
+const TURN_QUIET: u8 = 'q' as u8;
+
 #[entry]
 fn main() -> ! {
     let device = hal::stm32f30x::Peripherals::take().unwrap();
+    let core = cortex_m::Peripherals::take().unwrap();
     let mut rcc = device.RCC.constrain();
     let mut flash = device.FLASH.constrain();
     let clocks = rcc
@@ -31,11 +39,16 @@ fn main() -> ! {
     let gpioa = device.GPIOA.split(&mut rcc.ahb);
     let mut serial = device
         .USART1
-        .serial((gpioa.pa9, gpioa.pa10), Bps(9600), clocks);
+        .serial((gpioa.pa9, gpioa.pa10), Bps(115200), clocks);
+    let ser_int = serial.get_interrupt();
     serial.listen(serial::Event::Rxne);
-    let (tx, mut rx) = serial.split();
-
-    let mut l = Logger { tx };
+    let (mut tx, rx) = serial.split();
+    tx.write(0x00).unwrap();
+    unsafe {
+        L = Some(Logger { tx });
+        RX = Some(rx);
+    };
+    let l = unsafe { extract(&mut L) };
     write!(l, "\r\nVL53L0x demo\r\n");
 
     // i2c
@@ -43,26 +56,35 @@ fn main() -> ! {
     let scl = gpiob.pb8.alternating(gpio::AF4);
     let sda = gpiob.pb9.alternating(gpio::AF4);
     let i2c = device.I2C1.i2c((scl, sda), 1.mhz(), clocks);
-
-    let mut tof = vl53l0x::VL53L0x::new(i2c).unwrap();
-    write!(l, "WHO_AM_I: {}\r\n", tof.who_am_i());
-    write!(l, "RevisionID: {}\r\n", tof.revision_id);
-
+    write!(l, "\ri2c\r\n");
+    let mut tof = vl53l0x::VL53L0x::new(i2c).expect("vl");
+    write!(l, "vl53l0x ok\r\n");
+    unsafe { cortex_m::interrupt::enable() };
+    let mut nvic = core.NVIC;
+    nvic.enable(ser_int);
+    write!(l, "ready to set meas budget \r\n");
+    tof.set_measurement_timing_budget(200000).expect("timbudg");
+    write!(l, "meas budget set; start cont \r\n");
+    tof.start_continuous(0).expect("start cont");
+    write!(l, "All ok; Press 'q' to toggle verbosity!\r\n");
     loop {
-        match rx.read() {
-            Ok(b) => {
-                l.tx.write(b).unwrap();
+        match tof.read_range_continuous_millimeters() {
+            Ok(meas) => {
+                if unsafe { !QUIET } {
+                    write!(l, "vl: millis {}\r\n", meas);
+                }
             }
-            Err(nb::Error::Other(e)) => match e {
-                serial::Error::Overrun => {
-                    rx.clear_overrun_error();
-                }
-                _ => {
-                    write!(l, "read error: {:?}", e);
-                }
-            },
-            Err(nb::Error::WouldBlock) => {}
+            Err(e) => {
+                write!(l, "Err meas: {:?}\r\n", e);
+            }
         };
+    }
+}
+
+unsafe fn extract<T>(opt: &'static mut Option<T>) -> &'static mut T {
+    match opt {
+        Some(ref mut x) => &mut *x,
+        None => panic!("extract"),
     }
 }
 
@@ -94,6 +116,39 @@ impl<W: ehal::serial::Write<u8>> fmt::Write for Logger<W> {
     }
 }
 
+interrupt!(USART1_EXTI25, usart_exti25);
+fn usart_exti25() {
+    let rx = unsafe { extract(&mut RX) };
+    let l = unsafe { extract(&mut L) };
+    match rx.read() {
+        Ok(b) => {
+            if b == TURN_QUIET {
+                unsafe {
+                    QUIET = !QUIET;
+                }
+            } else {
+                // echo byte as is
+                write!(l, "{}", b as char);
+            }
+        }
+        Err(nb::Error::WouldBlock) => {}
+        Err(nb::Error::Other(e)) => match e {
+            serial::Error::Overrun => {
+                rx.clear_overrun_error();
+            }
+            serial::Error::Framing => {
+                rx.clear_framing_error();
+            }
+            serial::Error::Noise => {
+                rx.clear_noise_error();
+            }
+            _ => {
+                write!(l, "read error: {:?}", e);
+            }
+        },
+    };
+}
+
 #[exception]
 fn HardFault(ef: &ExceptionFrame) -> ! {
     panic!("HardFault at {:#?}", ef);
@@ -102,4 +157,40 @@ fn HardFault(ef: &ExceptionFrame) -> ! {
 #[exception]
 fn DefaultHandler(irqn: i16) {
     panic!("Unhandled exception (IRQn = {})", irqn);
+}
+
+#[panic_handler]
+fn panic(panic_info: &PanicInfo) -> ! {
+    match unsafe { &mut L } {
+        Some(ref mut l) => {
+            let payload = panic_info.payload().downcast_ref::<&str>();
+            match (panic_info.location(), payload) {
+                (Some(location), Some(msg)) => {
+                    write!(
+                        l,
+                        "\r\npanic in file '{}' at line {}: {:?}\r\n",
+                        location.file(),
+                        location.line(),
+                        msg
+                    );
+                }
+                (Some(location), None) => {
+                    write!(
+                        l,
+                        "panic in file '{}' at line {}",
+                        location.file(),
+                        location.line()
+                    );
+                }
+                (None, Some(msg)) => {
+                    write!(l, "panic: {:?}", msg);
+                }
+                (None, None) => {
+                    write!(l, "panic occured, no info available");
+                }
+            }
+        }
+        None => {}
+    }
+    unsafe { intrinsics::abort() }
 }
