8275b36b37cdcdb3a5023d4563b156ce576e307a
Alexander Zhuravlev <123368+bofh@users.noreply.github.com>
2018-10-31 21:17:38 +0100

    Run altitude EKF in loop

--- altitude/main.rs
+++ altitude/main.rs
@@ -1,5 +1,14 @@
 mod altitude;
+use libm;
+use nalgebra as na;
 
 fn main() {
-    let _ = altitude::ASL_EKF::new();
+    let mut ekf = altitude::ASL_EKF::new();
+    let baro_base = 97420.0;
+    for i in 0..100 {
+        let height:f32 = 100.0 + 50.0 * libm::sinf((i as f32) * 3.14 / 180.0);
+        let pressure = baro_base - height;
+        let fused = ekf.step(na::Vector2::new(pressure, height));
+        println!("Pressure: {}, height: {}, fused: {}", pressure, height, fused[0]);
+    }
 }
