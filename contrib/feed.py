76f30a1ac10d92878e72f70b05515bdb6c452ab2
Alexander Zhuravlev <123368+bofh@users.noreply.github.com>
2019-05-27 17:27:59 +0200

    Add script to feed the feed example

---
+++ contrib/feed.py
@@ -0,0 +1,14 @@
+import serial
+import sys
+import time
+
+port = serial.Serial(sys.argv[1], baudrate=460800, timeout=3)
+
+while True:
+    i = input().strip()
+    port.write(i.encode('ascii') + b'\n')
+    line = port.read_until()
+    if line.strip():
+        print(line.strip().decode('ascii', 'ignore'))
+    else:
+        print("fail")
--- feed/Readme.md
+++ feed/Readme.md
@@ -1,3 +1,12 @@
 # Dma commands
 
 Commands via dma (only write is dma powered now).
+
+# Testing
+
+Use feed.py provided in a contrib folder.
+
+Example:
+```bash
+cat file.log | python feed.py /dev/ttyACM0
+```
