f90e0b5700efa18adcd038bdce3530c09f30e051
Roma Sokolov <sokolov.r.v@gmail.com>
2023-07-15 22:44:13 +0200

    Add embassy mpu loop

--- embassy_raw_sensors/main.rs
+++ embassy_raw_sensors/main.rs
@@ -4,208 +4,180 @@
 #![feature(core_intrinsics)]
 #![feature(type_alias_impl_trait)]
 
-// # use core::fmt::Write;
-use defmt::*;
+use asm_delay::AsmDelay;
+use defmt;
+use core::fmt::Write;
 use {defmt_rtt as _, panic_probe as _};
-// use asm_delay::AsmDelay;
-use cortex_m_rt::entry;
 
-// use embassy_executor::Spawner;
-use embassy_executor::Executor;
-
-use embassy_stm32::time::Hertz;
+use embassy_executor::Spawner;
+use heapless::String;
 use embassy_stm32::dma::NoDma;
-use embassy_stm32::{bind_interrupts, peripherals, usart};
-use static_cell::StaticCell;
-// use mpu9250::Mpu9250;
+use embassy_time::Instant;
+use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_stm32::time::{mhz, Hertz};
+use embassy_stm32::{bind_interrupts, peripherals, spi, usart};
+use mpu9250::Mpu9250;
+use embassy_sync::signal::Signal;
+use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
 
-// static mut L: Option<hal::serial::Tx<hal::pac::USART2>> = None;
-// static mut RX: Option<hal::serial::Rx<hal::pac::USART2>> = None;
-static mut QUIET: bool = true;
-static mut NOW_MS: u32 = 0;
-const TURN_QUIET: u8 = 'q' as u8;
 
+struct ToggleQuiet;
+static QUIET: Signal<CriticalSectionRawMutex, ToggleQuiet> = Signal::new();
+const TOGGLE_QUIET: u8 = 'q' as u8;
 
 bind_interrupts!(struct Irqs {
     USART2 => usart::InterruptHandler<peripherals::USART2>;
 });
 
-static EXECUTOR: StaticCell<Executor> = StaticCell::new();
 
-#[entry]
-fn main() -> ! {
-    info!("Hello World!");
-
-    let executor = EXECUTOR.init(Executor::new());
-
-    executor.run(|spawner| {
-        unwrap!(spawner.spawn(main_task()));
-    })
+macro_rules! log_to_usart {
+    ($sink: ident, $buf: ident, $($args:tt)+) => ({
+        core::write!(&mut $buf, $($args)+).unwrap();
+        defmt::unwrap!($sink.blocking_write($buf.as_bytes()));
+        // TODO: don't clear until full? %)
+        $buf.clear();
+    });
 }
 
+
 #[embassy_executor::task]
-async fn main_task() -> ! {
+async fn reader(mut rx: usart::UartRx<'static, peripherals::USART2, NoDma>) {
+    loop {
+        match rx.nb_read() {
+            Ok(b) => {
+                if b == TOGGLE_QUIET {
+                    QUIET.signal(ToggleQuiet);
+                } else {
+                    // echo byte as is
+                }
+            }
+            Err(nb::Error::WouldBlock) => {}
+            Err(nb::Error::Other(e)) => match e {
+                // seems embassy clears errors automatically
+                usart::Error::Overrun => {
+                }
+                usart::Error::Framing => {
+                }
+                usart::Error::Noise => {
+                }
+                _ => {
+                    defmt::error!("read error: {:?}", defmt::Debug2Format(&e));
+                    // TODO: log to usart too
+                }
+            },
+        };
+    }
+}
+
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) -> ! {
+    defmt::info!("Starting MPU Embassy demo!");
+
     let mut config = embassy_stm32::Config::default();
+    let sysclk = 64_000_000;
     config.rcc.hse = Some(Hertz(8_000_000));
-    config.rcc.sysclk = Some(Hertz(64_000_000));
+    config.rcc.sysclk = Some(Hertz(sysclk));
     config.rcc.pclk1 = Some(Hertz(32_000_000));
     config.rcc.pclk2 = Some(Hertz(32_000_000));
     let device = embassy_stm32::init(config);
 
+    let mut log_buf: String<128> = String::new();
+
     let mut usart_config = usart::Config::default();
+    usart_config.detect_previous_overrun = false;
     usart_config.baudrate = 460800;
-    let mut usart = usart::Uart::new(device.USART2,
-                                     device.PA15,
-                                     device.PA2,
-                                     Irqs, NoDma, NoDma,
-                                     usart_config);
-    unwrap!(usart.blocking_write(b"Hello Embassy World!\r\n"));
-    info!("wrote Hello, starting echo");
+    usart_config.assume_noise_free = true;
+    let usart = usart::Uart::new(
+        device.USART2,
+        device.PA15,
+        device.PA2,
+        Irqs,
+        NoDma,
+        NoDma,
+        usart_config,
+    );
+    let (mut tx, rx) = usart.split();
+    log_to_usart!(tx, log_buf, "usart ok!\r\n");
+    log_to_usart!(tx, log_buf, "starting USART interrupt reader task!\r\n");
+    defmt::unwrap!(spawner.spawn(reader(rx)));
 
-    let mut buf = [0u8; 1];
+    let spi = spi::Spi::new(
+        device.SPI1,
+        device.PB3, // scl_sck
+        device.PB5, // mosi
+        device.PB4, // miso
+        NoDma,
+        NoDma,
+        mhz(1),
+        spi::Config::default(),
+    );
+
+    log_to_usart!(tx, log_buf, "spi ok!\r\n");
+
+    let mut delay = AsmDelay::new(asm_delay::bitrate::Hertz(sysclk));
+    log_to_usart!(tx, log_buf, "delay ok!\r\n");
+
+    let ncs = Output::new(device.PB0, Level::High, Speed::Low);
+    let mut mpu = match Mpu9250::imu_default(spi, ncs, &mut delay) {
+        Ok(m) => m,
+        Err(e) => {
+            log_to_usart!(tx, log_buf, "mpu_err  {:?}!\r\n", e);
+            defmt::panic!("mpu init error: {:?}", defmt::Debug2Format(&e));
+        }
+    };
+    defmt::unwrap!(tx.blocking_write(b"mpu ok !\r\n"));
+
+    let accel_biases: [f32; 3] = match mpu.calibrate_at_rest(&mut delay) {
+        Ok(ab) => ab,
+        Err(e) => {
+            log_to_usart!(tx, log_buf, "mpu calib err  {:?}!\r\n", e);
+            defmt::panic!("mpu calib error: {:?}", defmt::Debug2Format(&e));
+        }
+    };
+    log_to_usart!(tx, log_buf, "calib ok  {:?}!\r\n", accel_biases);
+
+    let mut prev_t_ms = Instant::now().as_millis();
+
+    log_to_usart!(tx, log_buf, "All ok, now: {:?}; Press 'q' to toggle verbosity!\r\n",
+                  prev_t_ms);
+
+    // TODO: read mpu on interrupt; await signals here
+    let mut quiet = false;
     loop {
-        unwrap!(usart.blocking_read(&mut buf));
-        unwrap!(usart.blocking_write(&buf));
+        let t_ms = Instant::now().as_millis();
+        let dt_ms = t_ms.wrapping_sub(prev_t_ms);
+        prev_t_ms = t_ms;
+        match mpu.all::<[f32; 3]>() {
+            Ok(meas) => {
+                let gyro = meas.gyro;
+                let accel = [
+                    meas.accel[0] - accel_biases[0],
+                    meas.accel[1] - accel_biases[1],
+                    meas.accel[2] - accel_biases[2],
+                ];
+                if !quiet {
+                    log_to_usart!(tx,
+                                  log_buf,
+                        "IMU: t:{}ms; dt:{}ms; g({};{};{}); a({};{};{})\r\n",
+                        t_ms,
+                        dt_ms,
+                        gyro[0],
+                        gyro[1],
+                        gyro[2],
+                        accel[0],
+                        accel[1],
+                        accel[2]
+                    );
+                }
+                if QUIET.signaled() {
+                    quiet = !quiet;
+                    QUIET.reset();
+                }
+            }
+            Err(e) => {
+                log_to_usart!(tx, log_buf, "Err: {:?}; {:?}", t_ms, e);
+            }
+        }
     }
-
-    // let ser_int = serial.get_interrupt();
-    // serial.listen(serial::Event::Rxne);
-    // let (mut tx, rx) = serial.split();
-    // writeln!(tx, "tx ok").unwrap();
-    // unsafe {
-    //     L = Some(tx);
-    //     RX = Some(rx);
-    // };
-    // let l = unsafe { extract(&mut L) };
-    // writeln!(l, "logger ok").unwrap();
-    // // SPI1
-    // let ncs = gpiob.pb0.output().push_pull();
-    // let spi = device.SPI1.spi(
-    //     // scl_sck, ad0_sd0_miso, sda_sdi_mosi,
-    //     (gpiob.pb3, gpiob.pb4, gpiob.pb5),
-    //     mpu9250::MODE,
-    //     1.mhz(),
-    //     clocks,
-    // );
-    // writeln!(l, "spi ok").unwrap();
-    // let mut delay = AsmDelay::new(clocks.sysclk());
-    // writeln!(l, "delay ok").unwrap();
-    // let mut mpu = match Mpu9250::imu_default(spi, ncs, &mut delay) {
-    //     Ok(m) => m,
-    //     Err(e) => {
-    //         writeln!(l, "Mpu init error: {:?}", e).unwrap();
-    //         panic!("mpu err");
-    //     }
-    // };
-    // writeln!(l, "mpu ok").unwrap();
-    // let accel_biases: [f32; 3] = match mpu.calibrate_at_rest(&mut delay) {
-    //     Ok(ab) => ab,
-    //     Err(e) => {
-    //         writeln!(l, "Mpu calib error: {:?}", e).unwrap();
-    //         panic!("mpu err");
-    //     }
-    // };
-    // writeln!(l, "calibration ok: {:?}", accel_biases).unwrap();
-    // let mut syst = core.SYST;
-    // unsafe { cortex_m::interrupt::enable() };
-    // let reload = (clocks.sysclk().0 / 1000) - 1;
-    // syst.set_reload(reload);
-    // syst.clear_current();
-    // syst.enable_interrupt();
-    // syst.enable_counter();
-    // unsafe { cortex_m::peripheral::NVIC::unmask(ser_int) };
-
-    // let mut prev_t_ms = now_ms();
-    // let mut prev_s = prev_t_ms / 1000;
-    // write!(
-    //     l,
-    //     "All ok, now: {:?}; Press 'q' to toggle verbosity!\r\n",
-    //     prev_t_ms
-    // )
-    // .unwrap();
-    // loop {
-    //     let t_ms = now_ms();
-    //     let t_s = t_ms / 1000;
-    //     let passed = t_s != prev_s;
-    //     prev_s = t_s;
-    //     let dt_ms = t_ms.wrapping_sub(prev_t_ms);
-    //     prev_t_ms = t_ms;
-    //     match mpu.all::<[f32; 3]>() {
-    //         Ok(meas) => {
-    //             let gyro = meas.gyro;
-    //             let accel = [
-    //                 meas.accel[0] - accel_biases[0],
-    //                 meas.accel[1] - accel_biases[1],
-    //                 meas.accel[2] - accel_biases[2],
-    //             ];
-    //             if unsafe { !QUIET } || passed {
-    //                 write!(
-    //                     l,
-    //                     "IMU: t:{}ms; dt:{}ms; g({};{};{}); a({};{};{})\r\n",
-    //                     t_ms,
-    //                     dt_ms,
-    //                     gyro[0],
-    //                     gyro[1],
-    //                     gyro[2],
-    //                     accel[0],
-    //                     accel[1],
-    //                     accel[2]
-    //                 )
-    //                 .unwrap();
-    //             }
-    //         }
-    //         Err(e) => {
-    //             write!(l, "Err: {:?}; {:?}", t_ms, e).unwrap();
-    //         }
-    //     }
-    // }
 }
-
-// unsafe fn extract<T>(opt: &'static mut Option<T>) -> &'static mut T {
-//     match opt {
-//         Some(ref mut x) => &mut *x,
-//         None => defmt::panic!("extract"),
-//     }
-// }
-
-// #[interrupt]
-// fn USART2_EXTI26() {
-//     let rx = unsafe { extract(&mut RX) };
-//     let l = unsafe { extract(&mut L) };
-//     match rx.read() {
-//         Ok(b) => {
-//             if b == TURN_QUIET {
-//                 unsafe {
-//                     QUIET = !QUIET;
-//                 }
-//             } else {
-//                 // echo byte as is
-//                 write!(l, "{}", b as char).unwrap();
-//             }
-//         }
-//         Err(nb::Error::WouldBlock) => {}
-//         Err(nb::Error::Other(e)) => match e {
-//             serial::Error::Overrun => {
-//                 rx.clear_overrun_error();
-//             }
-//             serial::Error::Framing => {
-//                 rx.clear_framing_error();
-//             }
-//             serial::Error::Noise => {
-//                 rx.clear_noise_error();
-//             }
-//             _ => {
-//                 write!(l, "read error: {:?}", e).unwrap();
-//             }
-//         },
-//     };
-// }
-
-// fn now_ms() -> u32 {
-//     unsafe { core::ptr::read_volatile(&NOW_MS as *const u32) }
-// }
-
-// #[exception]
-// unsafe fn SysTick() {
-//     NOW_MS = NOW_MS.wrapping_add(1);
-// }
